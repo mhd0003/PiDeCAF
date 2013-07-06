39ef9c850a10f75402eaefde9c754adb887fc2e2
Victoria Wu <victoria.chen.wu@gmail.com>
2013-07-06 11:36:43 -0500

    Starting mavlink_read library

---
+++ au_uav_ros/include/au_uav_ros/mavlink_read.h
@@ -0,0 +1,16 @@
+#ifndef AU_MAVLINK_READ_H
+#define AU_MAVLINK_READ_H
+/*
+ * library for mavlink read, specific to our au_uav_ros stuff
+ * wheeee
+ */
+
+namespace au_uav_ros	{
+	namespace mav	{
+	
+	
+	
+	}//end mav
+}//end au_uav_ros
+
+#endif
