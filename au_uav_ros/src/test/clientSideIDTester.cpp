2a4ef6a47f7fe45dd8cc7647afeefb8195839341
Victoria Wu <victoria.chen.wu@gmail.com>
2013-07-10 11:57:41 -0400

    another test thingy for mover node

--- au_uav_ros/src/test/clientSideIDTester.cpp
+++ au_uav_ros/src/test/clientSideIDTester.cpp
@@ -6,9 +6,14 @@
 namespace {
 class test_id_srv : public :: testing::Test	{
 	protected:
+	
+	bool get(au_uav_ros::planeIDGetter::Request &req,
+			au_uav_ros::planeIDGetter::Response &res)	{
+			res.planeID = 9999;	
+	}
 
 	virtual void SetUp()	{
-		client = n.serviceClient<au_uav_ros::planeIDGetter>("getPlaneID");		
+		server = n.advertiseService("getPlaneID", &test_id_srv::get, this);		
 			
 	}
 
@@ -18,25 +23,19 @@ class test_id_srv : public :: testing::Test	{
 
 	//stuff
 	ros::NodeHandle n;
-	ros::ServiceClient client;
+	ros::ServiceServer server;
 
 };
 
 
 }
 
-TEST_F(test_id_srv, getPlaneID)	{
+TEST_F(test_id_srv, returnPlaneID)	{
 
 	//wow this is confusing.
 	//So, if the ardupilot port can't even be opened, the id will be returned as 0??? what's happening?
-
-	ros::Duration(30).sleep(); //The other callback is saying it's got 32, but I'm getting a FALSE for client call success.
-
-	au_uav_ros::planeIDGetter srv;
-	EXPECT_TRUE(client.call(srv)) << "Client Call DID NOT Succeed";
-	EXPECT_FALSE(srv.response.planeID == -1);
-	EXPECT_TRUE(srv.response.planeID == (long int)32);
-
+	ros::Duration(30).sleep(); 
+//	EXPECT_TRUE(
 	fprintf(stderr, "planeIDTESTER::Plane ID is %d", srv.response.planeID);
 
 }
